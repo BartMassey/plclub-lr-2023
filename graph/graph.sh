83f7fc0cec13b7a9566c9a598e750cd7f6b75e51
Bart Massey <bart@cs.pdx.edu>
2013-02-15 02:39:46 -0800

    fixed graph title

--- graph/graph.sh
+++ graph/graph.sh
@@ -2,7 +2,7 @@
 # Copyright © 2013 Bart Massey
 # Generate Data.List.Combinator dependency graph.
 sh mkmkgraph.sh ../Data-List-Combinator.hs >mkgraph.awk &&
-( echo "digraph G {" &&
+( echo "digraph \"Dependencies in Data.List.Combinator\" {" &&
   awk -f mkgraph.awk <../Data-List-Combinator.hs | ./nub &&
   echo "}" ) >graph.dot &&
 dot -Tsvg graph.dot >graph.svg
